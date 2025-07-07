7691088575c7d62a22384d906c2e28c17cfe610b
Harlee <hariharanpugazh@gmail.com>
2025-07-07 12:34:17 +0530

    Second Commit

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(%h^3rhkht08&^^37_zt4mz3u)!m0mas+$3l2l%!dr&qa708(^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',  
+    'rest_framework',  
+    'corsheaders', 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
