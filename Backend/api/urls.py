7de073fcdddb6e8531b00b8be8181de7f697393f
Vikrame-dev <eswaramoorthyeswaramoorthy03@gmail.com>
2025-07-16 09:17:13 +0530

    Fix:url's of api fixed with name

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('signup/', signup),
-    path('login/', login),
-    path('logout/', logout),
-    path('get-user/', get_user),
-    path('pair-partner/', pair_partner),
-    path('send-message/', send_message),
-    path('get-messages/', get_messages),
+    path('signup/', signup, name="signup"),
+    path('login/', login, name="login"),
+    path('logout/', logout, name="logout"),
+    path('get-user/', get_user, name="get_user"),
+    path('pair-partner/', pair_partner, name="pair_partner"),
+    path('send-message/', send_message, name="send_message"),
+    path('get-messages/', get_messages, name="get_messages"),
 ]
